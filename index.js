22b4457ea6b35f47f4a3a0b571d099d8ee41a160
AbdulAbrahams <abdulabrahams2002@gmail.com>
2022-11-23 16:18:14 +0200

    update

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
-let display = document.getElementById('calculation');
+// let display = document.getElementById('calculation');
 
-let buttons = Array.from(document.getElementsByClassName('button'));
+// let buttons = Array.from(document.getElementsByClassName('button'));
+// let numbers = Array.from(document.getElementsByClassName('number'));
+// let clears = document.getElementsByClassName('clear');
+// let equals = document.querySelector('equal');
 
-buttons.map( button => {
-    button.addEventListener('click', (e) =>{
-       switch(e.target.innerText) {
-        default:
-            display.innerText += e.target.innerText;
-       }
-    });
-});
+// buttons.map( button => {
+//     button.addEventListener('click', (e) =>{
+//        switch(e.target.innerText) {
+
+//         case('C'):
+//             display.innerText = '';
+//             break
+        
+//         default:
+//             display.innerText += e.target.innerText;
+//        }
+//     });
+// });
+
+
+// // equals.addEventListener('click', function(e){
+// //     if(display.value === ''){
+// //         display.value = "";
+// //     }else{
+// //         let answer = eval(display.value);
+// //         display.value = answer;
+// //     }
+// // })
