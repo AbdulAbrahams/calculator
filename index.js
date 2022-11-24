588c302c515afa2c8906ef1c142505424d2d152b
AbdulAbrahams <abdulabrahams2002@gmail.com>
2022-11-24 11:41:23 +0200

    java

--- index.js
+++ index.js
@@ -1,30 +1,44 @@
-// let display = document.getElementById('calculation');
-
-// let buttons = Array.from(document.getElementsByClassName('button'));
-// let numbers = Array.from(document.getElementsByClassName('number'));
-// let clears = document.getElementsByClassName('clear');
-// let equals = document.querySelector('equal');
-
-// buttons.map( button => {
-//     button.addEventListener('click', (e) =>{
-//        switch(e.target.innerText) {
-
-//         case('C'):
-//             display.innerText = '';
-//             break
-        
-//         default:
-//             display.innerText += e.target.innerText;
-//        }
-//     });
-// });
+let calculate = document.getElementById('calculation');
+let answers = document.getElementById('output');
+let but = Array.from(document.getElementsByClassName('button'));
+let sum = '';
 
 
-// // equals.addEventListener('click', function(e){
-// //     if(display.value === ''){
-// //         display.value = "";
-// //     }else{
-// //         let answer = eval(display.value);
-// //         display.value = answer;
-// //     }
-// // })
+
+function evaluateSum() {
+    but.map( button => {
+    button.addEventListener('click', (e) =>{
+       switch(e.target.innerText) {
+        case('='):
+        output.innerText = eval(calculate.innerText);
+            break
+            case('←'):
+            calculate.innerText = calculate.innerText.slice(0,-1);
+                break
+            default:
+            calculate.innerText += e.target.innerText;
+       }
+    });
+});
+  }
+  evaluateSum();
+
+
+function clearSum(){
+    but.map( button => {
+        button.addEventListener('click', (e) =>{
+           switch(e.target.innerText) {
+    case('C'):
+        calculate.innerText = '';
+        answers.innerText = '';
+            break
+       }
+    })
+})
+};
+
+clearSum();
+
+  
+
+
